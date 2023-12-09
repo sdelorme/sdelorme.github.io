3cde51efa6cfd2b3bec867e4be95d07eec03a5b5
Seth Delorme <seth.delorme@gambyt.com>
2023-12-08 22:32:36 -0500

    placeholder under construction message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,12 @@
 import React from 'react'
-import {
-  AboutSection,
-  ArticlesSection,
-  ContactSection,
-  HeroSection,
-  InterestsSection,
-  Page,
-  ProjectsSection,
-  Seo,
-} from 'gatsby-theme-portfolio-minimal'
+import { Page, Seo } from 'gatsby-theme-portfolio-minimal'
 
 export default function IndexPage() {
   return (
     <>
       <Seo title="Seth Delorme" />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection
-          sectionId="articles"
-          heading="Latest Articles"
-          sources={['Blog']}
-        />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        {'Currently under construction.......awkward....brb....I promise'}
       </Page>
     </>
   )
